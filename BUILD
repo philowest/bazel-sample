7966862b6402c3d8fca777d6d27cd9a9f5ad2397
Phil West <philowest@gmail.com>
2015-09-14 15:36:53 +0200

    A working bazel build

---
+++ BUILD
@@ -0,0 +1,5 @@
+java_binary(
+    name = "echo",
+    srcs = glob(["**/*.java"]),
+    main_class = "west.Echo",
+)
---
+++ src/main/java/west/Echo.java
@@ -0,0 +1,11 @@
+package west;
+
+public class Echo {
+    public static void main(String args[]) {
+        echo(args[0]);
+    }
+
+    public static void echo(String sound) {
+        System.out.println(sound);
+    }
+}
